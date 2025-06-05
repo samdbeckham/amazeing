f0110896bb402c5d2b78500c134b6ca0420b677a
samdbeckham <sam@beckham.io>
2025-06-05 13:28:26 +0100

    Adds line drawing abiliites and fixes the canvas

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, pointA, pointB):
+        self.pointA = pointA
+        self.pointB = pointB
+
+    def draw(self, canvas, fill):
+        canvas.create_line(
+            self.pointA.x,
+            self.pointA.y,
+            self.pointB.x,
+            self.pointB.y,
+            fill=fill,
+            width=2
+        )
+
 class Window:
     def __init__(self, width, height):
-        root = Tk()
-        root.title = "Some kind of title"
-        self.__root = root
+        self.__root = Tk()
+        self.__root.title("Some kind of title")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-        canvas = Canvas()
+        canvas = Canvas(self.__root, bg="white", height=height, width=width)
         canvas.pack()
         self.canvas = canvas
 
@@ -23,11 +43,21 @@ class Window:
 
     def close(self):
         self.running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
+    def draw_line(self, line, fill):
+        line.draw(self.canvas, fill)
 
 def main():
     win = Window(800, 600)
+    pointA = Point(50,50)
+    pointB = Point(400,50)
+    pointC = Point(50,400)
+    lineAB = Line(pointA, pointB)
+    lineBC = Line(pointC, pointB)
+    lineCA = Line(pointA, pointC)
+    win.draw_line(lineAB, "black")
+    win.draw_line(lineBC, "red")
+    win.draw_line(lineCA, "black")
     win.wait_for_close()
 
 if __name__ == '__main__':
